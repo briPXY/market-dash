1d50c1f59b7837adf406a974c3f336baf6da1eea
briPXY <bryndood@gmail.com>
2025-03-28 16:34:05 +0700

    fix importing platform config

--- fastify-server.js
+++ fastify-server.js
@@ -1,46 +1,46 @@
 import Fastify from "fastify";
 const fastify = Fastify({ logger: true });
 
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
- 
-let PORT = 3000; // Default local port
-let HOST = '0.0.0.0'; // Allow external access
+import dotenv from "dotenv";
+dotenv.config();
 
-try {
-    // eslint-disable-next-line no-undef
-    const config = require("platformsh-config").config();
-    PORT = config.port; // Use Platform.sh assigned port
-} catch (err) {
-    console.log("Platform.sh config not found. Running in LOCAL MODE. \n", err.toString().slice(0,0));
-}
+import platformshConfig from 'platformsh-config';
+
+import path from 'node:path';
+
+import { fileURLToPath } from 'node:url';
 
 // Convert __dirname to work with ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import reqOHLC from "./src/backend/historyIO.js";
-
-// Enable CORS so the frontend can talk to this backend
-fastify.register(await import("@fastify/cors"), {
-    origin: "*"
-});
-
-await fastify.register(reqOHLC);
-
 fastify.register(await import('@fastify/static'), {
     root: path.join(__dirname, 'dist'),
 });
 
+fastify.register(await import("@fastify/cors"), {
+    origin: "*"
+});
+
+import reqOHLC from "./src/backend/historyIO.js";
+await fastify.register(reqOHLC);
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.LOCAL_DEV ? 3000 : platformshConfig.config().port;
+const HOST = '0.0.0.0'; // Ensure external access
+
+// eslint-disable-next-line no-undef
+console.log('\x1b[36m%s\x1b[0m', `Starting server on port ${PORT} (Local: ${!!process.env.LOCAL_DEV})`);
+
 fastify.get('/', function (req, reply) {
-    reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+    reply.header('Cache-Control', 'public, max-age=300');
+    reply.sendFile('index.html') 
 })
 
 
 // Start the proxy server
-fastify.listen({ port: PORT, host: HOST }, (err, address) => {
+fastify.listen({ port: PORT, host: HOST }, (err) => {
     if (err) {
-        fastify.log.error(err); 
+        fastify.log.error(err);
     }
-    console.log(`Fastify server running at ${address}`);
 });
