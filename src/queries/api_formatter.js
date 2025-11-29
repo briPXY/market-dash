d990461b9072b15083a036a9355630c924529db6
briPXY <bryndood@gmail.com>
2025-11-29 20:36:29 +0700

    add Uniswap v4 SDK

--- src/queries/api_formatter.js
+++ src/queries/api_formatter.js
@@ -3,7 +3,7 @@ import { WSS_DOMAIN } from "../constants/environment";
 function binance(base = "USDT", symbol = "ETH", range = "50"){
     const result = {}
     result.hour24 = `https://api.binance.com/api/v3/klines?symbol=${symbol.toUpperCase()}${base.toUpperCase()}&interval=5m&limit=288`
-    result.historical = `https://api.binance.com/api/v3/klines?symbol=${symbol.toUpperCase()}${base.toUpperCase()}&interval=${range}&limit=300`;
+    result.historical = `https://api.binance.com/api/v3/klines?symbol=${symbol.toUpperCase()}${base.toUpperCase()}&interval=${range}&limit=500`;
     result.index = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}${base.toLowerCase()}@indexPrice`;
     result.mark = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}${base.toLowerCase()}@markPrice`;
     result.trade = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}${base.toLowerCase()}@trade`;
