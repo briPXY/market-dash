4e090ad6f45efbb7228848b6bc8fa356589dfecc
briPXY <bryndood@gmail.com>
2025-12-09 01:43:48 +0700

    refactor: move initial pair data

--- src/constants/sourceConst.js
+++ src/constants/sourceConst.js
@@ -2,16 +2,15 @@ import { getPriceFromSqrtPriceX96 } from "../utils/price.math";
 import { binance_24h, UniswapV3_24h } from "../queries/fetch24hour";
 import { binance, UniswapV3 } from "../queries/fetchHistory";
 import { binanceTicker, UniswapV3BulkPrice } from "../queries/livePrice";
-import { binanceInitialTicks } from "./binanceTicks";
+import { binanceHighlights, uniswapV3EtherumHighlights, uniswapV3SepoilaStarterPairs } from "./starterPairs";
 import { WSS_DOMAIN } from "./environment";
 import { initData, initToken } from "./initData";
 import { getUniswapQuoteFromContract, getUniswapQuoteQueryFn, initDummy } from "../queries/quotes";
-import { checkBinanceSymbolAvailability } from "../queries/api_formatter";
 
 export const SourceConst = {};
 
 // uniswap v3
-SourceConst.UniswapV3 = {
+SourceConst["uniswap:1"] = { // name must resemble indexeDB's pair-list
     name: "UniswapV3",
     desc: "Uniswap V3 Ethereum",
     exchangeIcon: "uniswap",
@@ -19,7 +18,7 @@ SourceConst.UniswapV3 = {
     isDex: true,
     poolURL: "https://app.uniswap.org/explore/pools/ethereum/",
     intervals: ["1h", "1d"],
-    info: null,
+    initPairs: uniswapV3EtherumHighlights,
     bulkPrices: UniswapV3BulkPrice,
     livePrice: UniswapV3BulkPrice,
     ohlcFetch: UniswapV3,
@@ -27,11 +26,10 @@ SourceConst.UniswapV3 = {
     getPriceURL: (poolAddress) => `${WSS_DOMAIN}/liveprice/UniswapV3/${poolAddress}`,
     priceConverter: getPriceFromSqrtPriceX96,
     quoteFunction: getUniswapQuoteFromContract,
-    symbolCheck: () => null,
 };
 
 // uniswap Sepolia testnet
-SourceConst.UniswapV3Sepolia = {
+SourceConst["uniswap:11155111"] = {
     name: "UniswapV3Sepolia",
     desc: "Uniswap V3 Sepolia (Testnet)",
     exchangeIcon: "uniswap",
@@ -39,7 +37,7 @@ SourceConst.UniswapV3Sepolia = {
     isDex: true,
     poolURL: "https://app.uniswap.org/explore/pools/ethereum_sepolia/",
     intervals: ["1h", "1d"],
-    info: null,
+    initPairs: uniswapV3SepoilaStarterPairs,
     bulkPrices: UniswapV3BulkPrice,
     livePrice: UniswapV3BulkPrice,
     ohlcFetch: UniswapV3,
@@ -47,10 +45,9 @@ SourceConst.UniswapV3Sepolia = {
     priceConverter: getPriceFromSqrtPriceX96,
     getPriceURL: (poolAddress) => `${WSS_DOMAIN}/liveprice/UniswapV3Sepolia/${poolAddress}`,
     quoteFunction: getUniswapQuoteQueryFn,
-    symbolCheck: ()=>null,
 };
 
-// Binance (this is CEX network not L2 chain or BSC)
+// Binance (this is CEX price source not L2 chain like BSC)
 SourceConst.binance = {
     name: "binance",
     desc: "Binance CEX",
@@ -58,7 +55,7 @@ SourceConst.binance = {
     network: "binance-smart-chain",
     isDex: false,
     intervals: ["1m", "5m", "15m", "1h", "4h", "1d", "1w", "1M"],
-    info: binanceInitialTicks,
+    initPairs: binanceHighlights,
     livePrice: binanceTicker,
     ohlcFetch: binance,
     h24Query: binance_24h,
@@ -69,10 +66,9 @@ SourceConst.binance = {
     priceConverter: (p) => p,
     swappedSymbols: [],
     quoteFunction: initDummy,
-    symbolCheck: checkBinanceSymbolAvailability,
 };
 
-// Initial network
+// Initial (dummy) network
 SourceConst.init = {
     name: "init",
     desc: "Loading Network",
@@ -81,7 +77,7 @@ SourceConst.init = {
     isDex: false,
     poolURL: "/",
     intervals: ["1h", "1d"],
-    info: initToken,
+    initPairs: initToken,
     bulkPrices: () => "",
     livePrice: () => "0",
     ohlcFetch: async () => initData,
@@ -89,5 +85,4 @@ SourceConst.init = {
     priceConverter: () => "",
     getPriceURL: () => "init",
     quoteFunction: initDummy,
-    symbolCheck: checkBinanceSymbolAvailability,
 }
---
+++ src/constants/starterPairs.js
@@ -0,0 +1,617 @@
+export const binanceHighlights = [
+    {
+        "symbols": "BTCUSDT",
+        "token1": {
+            "decimals": 8,
+            "name": "Bitcoin",
+            "symbol": "BTC"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "ETHUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "Ethereum",
+            "symbol": "ETH"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "BNBUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "BNB",
+            "symbol": "BNB"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "XRPUSDT",
+        "token1": {
+            "decimals": 6,
+            "name": "XRP",
+            "symbol": "XRP"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "DOGEUSDT",
+        "token1": {
+            "decimals": 8,
+            "name": "Dogecoin",
+            "symbol": "DOGE"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "ADAUSDT",
+        "token1": {
+            "decimals": 6,
+            "name": "Cardano",
+            "symbol": "ADA"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "SOLUSDT",
+        "token1": {
+            "decimals": 9,
+            "name": "Solana",
+            "symbol": "SOL"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "DOTUSDT",
+        "token1": {
+            "decimals": 10,
+            "name": "Polkadot",
+            "symbol": "DOT"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "MATICUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "Polygon",
+            "symbol": "MATIC"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "LTCUSDT",
+        "token1": {
+            "decimals": 8,
+            "name": "Litecoin",
+            "symbol": "LTC"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "TRXUSDT",
+        "token1": {
+            "decimals": 6,
+            "name": "TRON",
+            "symbol": "TRX"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "SHIBUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "Shiba Inu",
+            "symbol": "SHIB"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "AVAXUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "Avalanche",
+            "symbol": "AVAX"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "LINKUSDT",
+        "token1": {
+            "decimals": 18,
+            "name": "Chainlink",
+            "symbol": "LINK"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "ATOMUSDT",
+        "token1": {
+            "decimals": 6,
+            "name": "Cosmos",
+            "symbol": "ATOM"
+        },
+        "token0": {
+            "decimals": 6,
+            "name": "USD Tether",
+            "symbol": "USDT"
+        }
+    }
+]
+
+export const uniswapV3EtherumHighlights = [
+    {
+        "symbols": "USDCWETH",
+        "feeTier": "500",
+        "address": "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640",
+        "liquidity": "1174022528522869165",
+        "token0": {
+            "decimals": "6",
+            "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "name": "USD Coin",
+            "symbol": "USDC"
+        },
+        "token1": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        }
+    },
+    {
+        "symbols": "WBTCWETH",
+        "feeTier": "3000",
+        "address": "0xcbcdf9626bc03e24f779434178a73a0b4bad62ed",
+        "liquidity": "90957880915521831",
+        "token0": {
+            "decimals": "8",
+            "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+            "name": "Wrapped BTC",
+            "symbol": "WBTC"
+        },
+        "token1": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        }
+    },
+    {
+        "symbols": "WETHUSDT",
+        "feeTier": "3000",
+        "address": "0x4e68ccd3e89f51c3074ca5072bbac773960dfa36",
+        "liquidity": "6212999787289401073",
+        "token0": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+            "name": "Tether USD",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "WBTCUSDC",
+        "feeTier": "3000",
+        "address": "0x99ac8ca7087fa4a2a1fb6357269965a2014abc35",
+        "liquidity": "3635699735871",
+        "token0": {
+            "decimals": "8",
+            "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+            "name": "Wrapped BTC",
+            "symbol": "WBTC"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "name": "USD Coin",
+            "symbol": "USDC"
+        }
+    },
+    {
+        "symbols": "WBTCUSDT",
+        "feeTier": "3000",
+        "address": "0x9db9e0e53058c89e5b94e29621a205198648425b",
+        "liquidity": "5986973195455",
+        "token0": {
+            "decimals": "8",
+            "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+            "name": "Wrapped BTC",
+            "symbol": "WBTC"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+            "name": "Tether USD",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "WETHUSDT2",
+        "feeTier": "500",
+        "address": "0x11b815efb8f581194ae79006d24e0d814b7697f6",
+        "liquidity": "677961259135891150",
+        "token0": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+            "name": "Tether USD",
+            "symbol": "USDT"
+        }
+    },
+    {
+        "symbols": "LINKWETH",
+        "feeTier": "3000",
+        "address": "0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8",
+        "liquidity": "985413440927803134482693",
+        "token0": {
+            "decimals": "18",
+            "address": "0x514910771af9ca656af840dff83e8264ecf986ca",
+            "name": "ChainLink Token",
+            "symbol": "LINK"
+        },
+        "token1": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        }
+    },
+    {
+        "symbols": "DAIWETH",
+        "feeTier": "3000",
+        "address": "0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8",
+        "liquidity": "87602362200522339900117",
+        "token0": {
+            "decimals": "18",
+            "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+            "name": "Dai Stablecoin",
+            "symbol": "DAI"
+        },
+        "token1": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        }
+    },
+    {
+        "symbols": "HKDMUSDM",
+        "feeTier": "100",
+        "address": "0x5796d7ad51583ae2c7297652edb7006bcd90519d",
+        "liquidity": "214907032744019640",
+        "token0": {
+            "decimals": "6",
+            "address": "0x0071f94350573cd411e40bb409e7ddd927224054",
+            "name": "HKD Mapped Token",
+            "symbol": "HKDM"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xbbaec992fc2d637151daf40451f160bf85f3c8c1",
+            "name": "USD Mapped Token",
+            "symbol": "USDM"
+        }
+    },
+    {
+        "symbols": "DAIUSDC",
+        "feeTier": "100",
+        "address": "0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+        "liquidity": "228971636403085695768182",
+        "token0": {
+            "decimals": "18",
+            "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+            "name": "Dai Stablecoin",
+            "symbol": "DAI"
+        },
+        "token1": {
+            "decimals": "6",
+            "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "name": "USD Coin",
+            "symbol": "USDC"
+        }
+    },
+    {
+        "symbols": "UNIWETH",
+        "feeTier": "3000",
+        "address": "0x1d42064fc4beb5f8aaf85f4617ae8b3b5b8bd801",
+        "liquidity": "71356745470601192883059",
+        "token0": {
+            "decimals": "18",
+            "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+            "name": "Uniswap",
+            "symbol": "UNI"
+        },
+        "token1": {
+            "decimals": "18",
+            "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        }
+    }
+];
+
+export const uniswapV3SepoilaStarterPairs = [
+    {
+        "feeTier": "500",
+        "id": "0x0a6886558f9f39876e47ec6ee1a521236e757388",
+        "token0": {
+            "decimals": "18",
+            "id": "0x634e1e79fad6fabfa17600fedc25d8dac22cf5eb",
+            "name": "Vampire Lord",
+            "symbol": "DRAC"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "10213",
+        "symbols": "DRACWETH"
+    },
+    {
+        "feeTier": "3000",
+        "id": "0x287b0e934ed0439e2a7b1d5f0fc25ea2c24b64f7",
+        "token0": {
+            "decimals": "18",
+            "id": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+            "name": "Uniswap",
+            "symbol": "UNI"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "140070",
+        "symbols": "UNIWETH"
+    },
+    {
+        "feeTier": "500",
+        "id": "0x3289680dd4d6c10bb19b899729cda5eef58aeff1",
+        "token0": {
+            "decimals": "6",
+            "id": "0x1c7d4b196cb0c7b01d743fbc6116a902379c7238",
+            "name": "USDC",
+            "symbol": "USDC"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "186102",
+        "symbols": "USDCWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0x58d850667c47981a1b6e7ca0b8dc2eb937cd4119",
+        "token0": {
+            "decimals": "6",
+            "id": "0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0",
+            "name": "USDT",
+            "symbol": "USDT"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "16065",
+        "symbols": "USDTWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0x634e1e35616f4b3ed8d3459e430900209ea7ca20",
+        "token0": {
+            "decimals": "6",
+            "id": "0x9a32e1383e7fd24a6d9c5db181457125a8d511c2",
+            "name": "faucet dao token",
+            "symbol": "FDT"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "2057",
+        "symbols": "FDTWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0x91bee36974282b9335f694035ca047309ea0b026",
+        "token0": {
+            "decimals": "18",
+            "id": "0xe0232d625ea3b94698f0a7dff702931b704083c9",
+            "name": "Yala Stable Coin",
+            "symbol": "YU"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "21295",
+        "symbols": "YUWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0xa1d509ef257220ce3bd5661d87849b83643dbea5",
+        "token0": {
+            "decimals": "18",
+            "id": "0x8137ee754f6c5c3cb66e9dd029b6d60c17f057f3",
+            "name": "ULTI",
+            "symbol": "ULTI"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "561",
+        "symbols": "ULTIWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0xa9df6536d606241e2708b3b6da63dd7163fb734d",
+        "token0": {
+            "decimals": "18",
+            "id": "0x4f7a67464b5976d7547c860109e4432d50afb38e",
+            "name": "Mainnet ETH",
+            "symbol": "METH"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0x517f2982701695d4e52f1ecfbef3ba31df470161",
+            "name": "tBTC v2",
+            "symbol": "tBTC"
+        },
+        "txCount": "16956",
+        "symbols": "METHtBTC"
+    },
+    {
+        "feeTier": "3000",
+        "id": "0xc0b6f0c5d4c33c59b4672000d490992b7097ba40",
+        "token0": {
+            "decimals": "18",
+            "id": "0xbbd3edd4d3b519c0d14965d9311185cfac8c3220",
+            "name": "Yala BTC",
+            "symbol": "YBTC"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xe0232d625ea3b94698f0a7dff702931b704083c9",
+            "name": "Yala Stable Coin",
+            "symbol": "YU"
+        },
+        "txCount": "28142",
+        "symbols": "YBTCYU"
+    },
+    {
+        "feeTier": "3000",
+        "id": "0xc4545a0b4a87cbaff03a73f45c377c0b5f416e00",
+        "token0": {
+            "decimals": "18",
+            "id": "0xbbd3edd4d3b519c0d14965d9311185cfac8c3220",
+            "name": "Yala BTC",
+            "symbol": "YBTC"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "139506",
+        "symbols": "YBTCWETH"
+    },
+    {
+        "feeTier": "10000",
+        "id": "0xfac1138c5a426d26a6e350868eee6501788f1417",
+        "token0": {
+            "decimals": "18",
+            "id": "0x810a3b22c91002155d305c4ce032978e3a97f8c4",
+            "name": "MON",
+            "symbol": "MON"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "48686",
+        "symbols": "MONWETH"
+    },
+    {
+        "feeTier": "3000",
+        "id": "0xfc5fab0a82d2601fd4a1a5f11a8979c8fc95ab8a",
+        "token0": {
+            "decimals": "18",
+            "id": "0x7352cdbca63f62358f08f6514d3b7ff2a2872aad",
+            "name": "BOYMAT",
+            "symbol": "BYT"
+        },
+        "token1": {
+            "decimals": "18",
+            "id": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
+            "name": "Wrapped Ether",
+            "symbol": "WETH"
+        },
+        "txCount": "4920",
+        "symbols": "BYTWETH"
+    }
+];
